a836a4c197c6809abeea5549567a6f6dc7db26a3
lautaropacella <59834503+lautaropacella@users.noreply.github.com>
2021-03-19 16:17:17 +0100

    Update Wpp_Analizer.py

--- Wpp_Analizer.py
+++ Wpp_Analizer.py
@@ -40,6 +40,7 @@ with st.beta_expander("¿Cómo Funciona?", expanded = False):
     """)
 
 upload_file = st.file_uploader("WhatsApp Chat", accept_multiple_files = False, type = 'txt')
+st.markdown('**EL CONTENIDO DE LA CONVERSACIÓN NO ES ACCESIBLE NI SERÁ GUARDADO**.')
 if upload_file:
     with st.spinner('Analizando Conversación'):
 
